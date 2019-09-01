92a4d58ca340cba5d743f7efb04ffbad9562015f
Dylan Romansky <dromansk@e1z2r3p20.42.us.org>
2019-08-31 22:56:29 -0700

    added my own prompt script dealy doodad

---
+++ prompt_bigdigsquig.sh
@@ -0,0 +1,15 @@
+# Default prompt theme
+export PROMPT_THEME="bigdigsquig"
+
+# Path of base directory (used in theme scripts to find bin folder)
+export PROMPT_BASE_DIR="$HOME/bin/ptheme"
+
+# Path of prompt theme directory (used to avoid rewriting the path)
+export PROMPT_THEME_DIR="$PROMPT_BASE_DIR/themes"
+
+prompt_command ()
+{
+    PS1="$("$PROMPT_THEME_DIR"/"$PROMPT_THEME" "$?")"
+}
+
+precmd_functions=(prompt_command)
